72d1ff5b31cbd470067a3d7d07d750e3bcf498be
unknown <gebril.gabriel@gmail.com>
2018-09-11 23:30:28 -0400

    fixed bug in keys

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const keys = require('../config/keys')
 
 const pool = new Pool({
-    user: keys.username,
+    user: keys.db.username,
     host: '127.0.0.1',
     database: 'aaw',
     password: keys.db.password
