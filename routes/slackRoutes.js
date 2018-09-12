9b9ee73c3660144d37f9af33a3dab99aae13f6ee
unknown <gebril.gabriel@gmail.com>
2018-09-11 23:17:33 -0400

    added a get route for testing

--- routes/slackRoutes.js
+++ routes/slackRoutes.js
@@ -3,6 +3,9 @@ var games = require("../controllers/gameController")
 var router = express.Router();
 
 router.post("/games", games.createGame);
+router.post('games', function (req, res) {
+    res.send("hit");
+});
 
 
 module.exports = router;
