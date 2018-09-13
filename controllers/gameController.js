16e36d3820e8e68b2c167fca4aee1b41876252f3
unknown <gebril.gabriel@gmail.com>
2018-09-13 14:13:16 -0400

    Create game fully implemented, error handling for when bot isn't in channel added

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -31,8 +31,10 @@ function shuffle(a) {
 
 exports.createGame = async function (req, res) {
 
-    res.send({ "response_type": "in_channel" });
-    console.log(req.body)
+    // res.send({ "response_type": "in_channel" });
+
+    console.log(req.body);
+
     const team_id = req.body.team_id;
     console.log(team_id);
     let token = await tokens.findByTokenID({ tokenID: team_id.toUpperCase() });
@@ -45,49 +47,61 @@ exports.createGame = async function (req, res) {
 
     //This slackbot is here to extend the normal bot
     const exBot = new Slack({ token: token.accesstoken });
+    const channel_id = req.body.channel_id;
+    let inC = true;
+    await exBot.chat.postMessage({ token: token.bottoken, channel: channel_id, as_user: true, username: 'overseer', text: `Creating game...` }).catch(err =>
+        inC = false
+    );
+    if (inC) {
+        res.send('');
+    } else {
+        res.send("Please invite @overseer to channel!")
+    }
 
 
-    exBot.channel.invite()
 
     // console.log(req.body);
-    const channel_id = req.body.channel_id;
+
 
     console.log(channel_id);
     // const channel_name = req.body.channel_name;
-    await games.create({ team: team_id }).catch(err => res.send("A game is already going on in your workspace."));
-    let assassins = await exBot.channels.info({ channel: channel_id });
-    assassins = assassins.channel.members;
-    for (let i = 0; i < assassins.length; i++) {
-        const userE = assassins[i];
-        let info = await exBot.users.info({ user: userE });
-        assassins[i] = { id: info.user.id, name: info.user.real_name, is_bot: info.user.is_bot };
-    }
-    assassins = assassins.filter(e => {
-        console.log(!e.is_bot);
-        return !e.is_bot;
-    });
-    console.log(assassins);
-    assassins = shuffle(assassins);
-    let targets = [];
-    for (let i = 0; i < assassins.length; i++) {
-        if (i < assassins.length - 1) {
-            targets[i] = assassins[i + 1];
-        } else {
-            targets[i] = assassins[0];
-        }
-    }
-    games.add({
-        team: team_id,
-        assassins: assassins,
-        targets: targets
-    }).then(res => {
+    if (inC) {
+        await games.create({ team: team_id }).catch(err => res.send("A game is already going on in your workspace."));
+        let assassins = await exBot.channels.info({ channel: channel_id });
+        assassins = assassins.channel.members;
         for (let i = 0; i < assassins.length; i++) {
-            // bot.postMessage(assassins[i].id, `your target is ${targets[i].name}`)
-            exBot.chat.postMessage({ token: token.bottoken, channel: assassins[i].id, as_user: true, username: 'overseer', text: `your target is ${targets[i].name}` })
+            const userE = assassins[i];
+            let info = await exBot.users.info({ user: userE });
+            assassins[i] = { id: info.user.id, name: info.user.real_name, is_bot: info.user.is_bot };
         }
+        assassins = assassins.filter(e => {
+            console.log(!e.is_bot);
+            return !e.is_bot;
+        });
+        console.log(assassins);
+        assassins = shuffle(assassins);
+        let targets = [];
+        for (let i = 0; i < assassins.length; i++) {
+            if (i < assassins.length - 1) {
+                targets[i] = assassins[i + 1];
+            } else {
+                targets[i] = assassins[0];
+            }
+        }
+        games.add({
+            team: team_id,
+            assassins: assassins,
+            targets: targets
+        }).then(res => {
+            for (let i = 0; i < assassins.length; i++) {
+                // bot.postMessage(assassins[i].id, `your target is ${targets[i].name}`)
+                exBot.chat.postMessage({ token: token.bottoken, channel: assassins[i].id, as_user: true, username: 'overseer', text: `your target is ${targets[i].name}` })
+            }
 
-    }).catch(err => console.log(err));
+        }).catch(err => exBot.chat.postMessage({ token: token.bottoken, channel: channel_id, as_user: true, username: 'overseer', text: `A game is already taking place in your workspace` }));
 
+
+    }
 
 
 
